c6ce81a3caeb40f3196ef6b945a68e25f16dc845
Habyte <qwaszxed1234@gmail.com>
2023-03-13 23:50:28 +0900

    Feat: "게시물 호버 이펙트 변경" 호버시 y축 translate 애니메이션 추가

--- src/components/Study/article.tsx
+++ src/components/Study/article.tsx
@@ -1,13 +1,13 @@
 export default function StudyArticle() {
   return (
-    <article className="p-4 flex max-w-xl flex-col items-start justify-between rounded-md border border-gray-600 hover:shadow-lg hover:ease-in duration-200">
+    <article className="p-4 flex max-w-xl flex-col items-start justify-between rounded-md border border-gray-600 hover:shadow-lg hover:ease-in duration-200 hover:-translate-y-1">
       <div className="flex items-center gap-x-4 text-xs">
         <time dateTime="2020-03-16" className="text-gray-500">
           Mar 16, 2020
         </time>
         <a
           href="#"
-          className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
+          className="relative z-1 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
         >
           Marketing
         </a>

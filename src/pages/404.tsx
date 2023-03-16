6e81df84f5d11633d3e5246952400d417291fbd6
Habyte <qwaszxed1234@gmail.com>
2023-03-16 16:34:21 +0900

    fix: "404페이지 홈으로 돌아가기 수정" 404 페이지에서 홈으로 돌아가기 버튼에 url 링크 기능 넣음

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <div className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
@@ -10,12 +12,12 @@ export default function Custom404() {
           죄송합니다, 요청한 페이지를 찾을 수 없습니다.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
+          <Link
+            href="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             홈으로 돌아가기
-          </a>
+          </Link>
         </div>
       </div>
     </div>

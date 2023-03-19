c328be278da49ab383e9de184cf357d496e484df
Habyte <qwaszxed1234@gmail.com>
2023-03-19 15:07:09 +0900

    feat: 텍스트 에디터 라이브러리 적용 포스트 게시물 작성시 본문 작성용 텍스트 에디터 라이브러리 적용

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -4,6 +4,8 @@ import RecruitInfo from "@/components/CreatPost/recruitInfo";
 import SingleSelectList from "@/components/OptionList/singleSelectList";
 import SearchStack from "@/components/SearchStack/searchStack";
 import UploadImage from "@/components/CreatPost/uploadImage";
+import TextEditor from "@/components/CreatPost/textEditor";
+import initialBodyText from "@/lib/initialBodyText";
 
 export default function CreatePost() {
   const recruitInfo = useRef<string[][] | null>([[]]);
@@ -13,6 +15,7 @@ export default function CreatePost() {
   const [location, setLocation] = useState("");
   const [stackList, setStackList] = useState<string[]>([]);
   const [image, setImage] = useState<File | null>(null);
+  const [bodyText, setBodyText] = useState<string>(initialBodyText);
   const [infoComp, setInfoComp] = useState<ReactElement[]>([
     <RecruitInfo
       key={0}
@@ -55,13 +58,12 @@ export default function CreatePost() {
   };
 
   const onSubmit = () => {
-    console.log(recruitInfo.current);
-    console.log(stackList);
+    console.log(bodyText);
   };
 
   return (
     <div className="isolate bg-white py-14 px-6 sm:py-20 lg:px-8">
-      <div className="mx-auto pb-8 max-w-2xl text-center border-b border-gray-200">
+      <div className="mx-auto pb-8 max-w-4xl text-center border-b border-gray-200">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           모임 생성
         </h2>
@@ -69,7 +71,7 @@ export default function CreatePost() {
           만들고 싶은 모임의 정보를 입력해주세요
         </p>
       </div>
-      <div className="mx-auto mt-10 max-w-xl sm:mt-14">
+      <div className="mx-auto mt-10 max-w-3xl sm:mt-14">
         <div className="grid grid-cols-1 gap-y-6 gap-x-8">
           <div className="sm:col-span-2">
             <label className="block text-sm font-semibold leading-6 text-gray-900">
@@ -201,19 +203,11 @@ export default function CreatePost() {
           </div>
 
           <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
+            <label className="block text-sm font-semibold leading-6 text-gray-900">
               프로젝트 설명 *
             </label>
             <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              ></textarea>
+              <TextEditor bodyText={bodyText} setBodyText={setBodyText} />
             </div>
           </div>
         </div>

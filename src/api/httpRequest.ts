6b1d9823e13a0fc300ebb577f638dac0f22a6082
Habyte <qwaszxed1234@gmail.com>
2023-04-06 23:42:40 +0900

    feat: 서버 https 적용으로 인한 주소 변경

--- src/api/httpRequest.ts
+++ src/api/httpRequest.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const HttpRequest = axios.create({
-  baseURL: "https://41a9-3-37-242-238.jp.ngrok.io",
+  baseURL: "https://6f89-3-37-242-238.jp.ngrok.io",
 });
 
 export default HttpRequest;

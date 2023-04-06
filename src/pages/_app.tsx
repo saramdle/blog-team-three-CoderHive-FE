7037cddd6baa49aa53fa6bcba06a4d3e9fd6abd6
Habyte <83758021+Gonggibab@users.noreply.github.com>
2023-04-06 23:04:45 +0900

    Revert "Feat: Mixed Content 에러 임시 수정"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,10 +13,6 @@ export default function App({ Component, ...rest }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Provider store={store}>
